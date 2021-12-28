c0a702df61749c238848483e24e9eaca12ee4a3c
Amine <ka_adjou@esi.dz>
2021-12-28 19:59:52 +0100

    1-Creating our express server

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const app = express(); 
+
+app.get('/', function (req,res) {
+    res.send('Helloworld'); 
+});
+
+app.listen(3000,()=>console.log('Server is listening on port 3000')); 
