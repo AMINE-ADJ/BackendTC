d7dc9e30a0ae6cfbf676235922f4dfd50563cb24
Amine AD <85430078+AMINE-ADJ@users.noreply.github.com>
2021-12-30 03:51:29 -0800

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from 'express';
-import https from 'https'; 
+// import https from 'https'; 
 import './core/db.js';
-
+import routers from './routers/index.js'; 
 const app = express(); 
+const port = 7000; 
 
-app.listen(3000,()=>console.log('Server is listening on port 3000')); 
+// app.get('/', function (req,res) {
+//     res.send('<h1> Html tags !! </h1>'); 
+// });
+// app.get('/about', function(req,res) {
+// res.send('Im Amine AD 2CP student at ESI ALGIERS !!'); 
+// }); 
+
+app.use('/', routers); 
+
+
+app.listen(port,()=>console.log(`Server is listening on port ${port}`)); 
 
