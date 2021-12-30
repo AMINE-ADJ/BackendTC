5e22be0a5b6c6abad7ac2f30c63a9002bc010891
Amine AD <85430078+AMINE-ADJ@users.noreply.github.com>
2021-12-29 16:54:00 -0800

    Init our DB with several collections

--- src/core/db.js
+++ src/core/db.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose"; //n'oublie pas d'installer mongoose. 
+import {init} from "./init.js"
 
 const username = "team01"; 
 const password ="team01"; 
 
 const dbUrl =`mongodb+srv://${username}:${password}@cluster0.mztwh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`; 
- // now init part. 
 
-mongoose.connect(dbUrl); 
-
+const promise = mongoose.connect(dbUrl); 
+promise.then(()=> {
+    init().then(()=>{
+        console.log('Initialisation completed !!');
+    })
+}).catch(err=> {
+    console.error(err.message);
+})
 
 mongoose.connection.on("connected", ()=>
     console.log('connected to MongoDB')
