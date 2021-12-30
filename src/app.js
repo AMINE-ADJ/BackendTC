c43005d1af298b5ec1b03dca2419239832dc1057
Amine AD <85430078+AMINE-ADJ@users.noreply.github.com>
2021-12-29 16:58:50 -0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,15 +4,5 @@ import './core/db.js';
 
 const app = express(); 
 
-// app.get('/', function (req,res) {
-//     res.send('<h1> Html tags !! </h1>'); 
-// });
-// app.get('/about', function(req,res) {
-// res.send('Im Amine AD 2CP student at ESI ALGIERS !!'); 
-// }); 
-
-app.use('/', routers); 
-
-
 app.listen(3000,()=>console.log('Server is listening on port 3000')); 
 
